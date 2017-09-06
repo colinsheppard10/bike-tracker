0d37c99011b3258f6c3d162ee63bdb5faf7e695e
Colin Sheppard <colinsheppard10@gmail.com>
2017-09-05 21:07:12 -0500

    ok

--- pi-monitoring/at_commands/mycom_edit.py
+++ pi-monitoring/at_commands/mycom_edit.py
@@ -1,22 +1,34 @@
-import time
+# import time
+# import serial
+
+# # configure the serial connections (the parameters differs on the device you are connecting to)
+# ser = serial.Serial(
+#     port='/dev/ttyUSB0',
+#     baudrate=9600,
+#     parity=serial.PARITY_NONE,
+#     stopbits=serial.STOPBITS_ONE,
+#     bytesize=serial.EIGHTBITS,
+#     timeout=1
+# )
+
+# ser.isOpen()
+# out = ''
+# print 'This program will continually loop looking for recieved messages'
+# while 1:
+#         while ser.inWaiting() > 0:
+#             out += ser.read(1)
+
+#         if out != '':
+#             print ">>" + out
+
 import serial
+import time
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-    port='/dev/ttyUSB0',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-)
-
-ser.isOpen()
-out = ''
-print 'This program will continually loop looking for recieved messages'
-while 1:
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
-
-        if out != '':
-            print ">>" + out
+if __name__ == '__main__':
+    ser = serial.Serial('COM3', baudrate=1000000)
+    data = []
+    time0 = time.time()
+    while (time.time() - time0 < 5):  # Read data for 5 seconds
+        data.append(ser.readline())
+    ser.close()
+    print ">>" + data
