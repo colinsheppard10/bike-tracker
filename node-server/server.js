ad45629cdbcb612533121493d3c735a6521a93ff
Colin <colin.sheppard@aeris.net>
2017-09-17 21:05:27 -0500

    socket addition

--- node-server/server.js
+++ node-server/server.js
@@ -1,29 +1,42 @@
-const express = require ('express');
+const express = require('express');
 const request = require('superagent');
 var path = require('path');
-const app = express.Router();
+const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
-const server = app.listen('8081', () =>{
-    console.log(`Server running at address: ${server.address().address} on port: ${server.address().port}`)
-})
+http.listen(8081, function () {
+    console.log('listening on *:8081');
+});
 
-app.get('/getLatLng',(req, res)=>{
+app.get('/getLatLng', (req, res) => {
     console.log('back at server calling AerFrame');
-    request.get('https://www.google.com/', (err, data) =>{
-        console.log(data); 
+    request.get('https://www.google.com/', (err, data) => {
+        console.log(data);
         // just here for testing
         // send request "request sent"  or AerFrame response
-        var response = { lat: 41.8993837, lng: -87.6641918 }; 
+        var response = { lat: 41.8993837, lng: -87.6641918 };
         res.send(response);
     })
 })
 
-app.post('/diplayLatLng', (req, res)=>{
-    console.log(req)
+app.post('/diplayLatLng', (req, res) => {
+    console.log(req);
 })
 
-app.get('/', (req, res) =>{
-    res.sendFile( __dirname + "/" + "public/index.html" );
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/" + "public/index.html");
 })
+
+app.get('/socket', (req, res) => {
+    res.sendFile(__dirname + "/" + "public/socket.html");
+})
+
+io.on('connection', function (socket) {
+    console.log(`connection recieved on: ${socket}`);
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
